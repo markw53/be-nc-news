fed8145ac293b85866dada5d2012e0bc11685c7a
markw53 <m.workman@outlook.com>
2024-10-17 14:53:50 +0100

    install cors

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const apiRouter = require("./routers/api-router");
-const cors = require("cors");
 const app = express();
-app.use(cors());
+const cors = require('cors');
 app.use(express.json());
+app.use(cors());
 
 const {
     handlesPSQLErrors,
