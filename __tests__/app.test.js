3ed2786a6877146c62f2cad47895bd9af5d2823e
markw53 <m.workman@outlook.com>
2024-10-17 17:17:16 +0100

    edited test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -603,18 +603,12 @@ describe('POST /api/topics', () => {
             .send(incompleteTopic)
             .expect(400)
             .then(({ body }) => {
-                expect(body.msg).toBe('Bad Request: Missing required fields');
+                expect(body.msg).toBe('Bad Request: Missing required fields "slug" and/or "description"');
             });
     });
 });
 
 describe('DELETE /api/articles/:article_id', () => {
-
-// it('204: deletes the article by id and responds with no content', () => {
-//     return request(app)
-//         .delete('/api/articles/1')
-//         .expect(204);
-// });
 
 it('400: responds with an error for Invalid article_id', () => {
     return request(app)
